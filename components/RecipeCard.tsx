f544af5e830f3fe0b71ced2e8e5bb898c073123d
jaydendeveloper <contact.jaydendev@gmail.com>
2025-10-02 10:59:47 +0200

    Added icon to starred recipes

--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -1,20 +1,25 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, type PressableProps } from "react-native";
 
 const RecipeCard = ({
 	title,
 	ingredients,
 	instructions,
+	isStarred,
+	...props
 }: {
 	title: string;
 	ingredients: string;
 	instructions: string;
-}) => {
+	isStarred: boolean;
+} & PressableProps) => {
 	return (
-		<View className="border p-4 m-2 rounded">
-			<Text className="text-xl font-bold">{title}</Text>
-			<Text className="text-gray-600">Ingredients: {ingredients}</Text>
-			<Text className="text-gray-600">Instructions: {instructions}</Text>
-		</View>
+		<Pressable
+			{...props}
+			className=" flex flex-row justify-between border p-4 m-2 rounded"
+		>
+			<Text className="text-2xl font-bold">{title}</Text>
+			{isStarred ? <Text className="text-2xl">⭐</Text> : null}
+		</Pressable>
 	);
 };
 
