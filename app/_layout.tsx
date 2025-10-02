77b77cf146787b82bfe9fbd2a7632cbc43d4d882
jaydendeveloper <contact.jaydendev@gmail.com>
2025-10-02 16:18:21 +0200

    Removed View from root which caused the app to not start

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,6 @@ import { initDb } from "@/helpers/db";
 import { Stack } from "expo-router";
 import { SQLiteProvider } from "expo-sqlite";
 import { Suspense } from "react";
-import { View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css";
 
@@ -12,9 +11,7 @@ export default function RootLayout() {
 		<SafeAreaProvider>
 			<Suspense fallback={<Loading />}>
 				<SQLiteProvider onInit={initDb} databaseName="database" useSuspense>
-					<View className="dark:bg-zinc-900">
-						<Stack screenOptions={{ headerShown: false }} />
-					</View>
+					<Stack screenOptions={{ headerShown: false }} />
 				</SQLiteProvider>
 			</Suspense>
 		</SafeAreaProvider>
